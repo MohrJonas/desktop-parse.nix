0f8c7759ce9c105c091a1a35ab7516da320b9189
Jonas Mohr <git@al1as.me>
2025-12-23 22:52:20 +0100

    Initial commit

---
+++ default.nix
@@ -0,0 +1,36 @@
+lib:
+let
+  util = import ./util.nix lib;
+
+  parseDesktopFile = content:
+    let
+      lines = lib.strings.splitString "\n" content;
+      elements = builtins.filter
+        (element: element != null)
+        (builtins.map
+          (line:
+            if util.isSectionHeading line then
+              line
+            else if util.isEmptyLine line then
+              null
+            else if util.isContentLine line then
+              util.parseContentLine line
+            else
+              throw "Unknown line type ${line}")
+          lines);
+    in
+      (builtins.foldl'
+        (state: element:
+          if builtins.isAttrs element then
+            { acc = state.acc // { ${state.key} =
+                (state.acc.${state.key} or []) ++ [element]; };
+              key = state.key; }
+          else
+            { acc = state.acc; key = element; })
+        { acc = {}; key = null; }
+        elements).acc;
+
+in {
+  parseDesktopFileString = content: parseDesktopFile content;
+  parseDesktopFilePath = path: parseDesktopFile (builtins.readFile path);
+}
---
+++ util.nix
@@ -0,0 +1,11 @@
+lib:
+{
+    isSectionHeading = line: builtins.substring 0 1 line == "[";
+    isEmptyLine = line: builtins.stringLength line == 0;
+    isContentLine = line: builtins.match ".*=.*" line != null;
+    parseContentLine = line:
+        let
+            parts = lib.strings.splitString "=" line;
+        in
+            { key = builtins.head parts; value = lib.last parts; };
+}
