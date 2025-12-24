84a96c971a4002be1772350db3f8ab6cce55ef58
Jonas Mohr <git@al1as.me>
2025-12-24 16:17:24 +0100

    Formatted code via alejandra

--- util.nix
+++ util.nix
@@ -1,12 +1,12 @@
-lib:
-{
-    isSectionHeading = line: builtins.substring 0 1 line == "[";
-    isEmptyLine = line: builtins.stringLength line == 0;
-    isContentLine = line: builtins.match ".*=.*" line != null;
-    parseContentLine = line:
-        let
-            parts = lib.strings.splitString "=" line;
-        in
-            { key = builtins.head parts; value = lib.last parts; };
-    trimSectionHeading = line: builtins.substring 1 ((builtins.stringLength line )- 2) line;
+lib: {
+  isSectionHeading = line: builtins.substring 0 1 line == "[";
+  isEmptyLine = line: builtins.stringLength line == 0;
+  isContentLine = line: builtins.match ".*=.*" line != null;
+  parseContentLine = line: let
+    parts = lib.strings.splitString "=" line;
+  in {
+    key = builtins.head parts;
+    value = lib.last parts;
+  };
+  trimSectionHeading = line: builtins.substring 1 ((builtins.stringLength line) - 2) line;
 }
